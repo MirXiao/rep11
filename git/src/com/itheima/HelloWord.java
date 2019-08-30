51d23ee433c800de071f046773e0dbb09560e840
yangyan <yangyan@.com>
2019-08-30 14:31:43 +0800

    six

--- git/src/com/itheima/HelloWord.java
+++ git/src/com/itheima/HelloWord.java
@@ -2,6 +2,6 @@ package com.itheima;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("git  hahaha hahah hahaha hhaa hello world");
+        System.out.println("git lals hahaha hahah hahaha hhaa hello world");
     }
 }
